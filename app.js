9ff49702058eb1976e30cc88b296188540b64fa3
Kawinphop Chomnikorn <kawinphop.ch@kkumail.com>
2022-04-28 09:24:44 +0700

    1

---
+++ app.js
@@ -0,0 +1,11 @@
+//แสดงข้อความในหน้าเว็บ
+document.write("<h1>กวินภพ.com</h1>"); 
+document.write("<p>Javascript_basic</p>");
+
+/*
+แสดงข้อความในแท็ป console ของเว็บ brower
+*/
+alert("คุณคิดอะไรอยู่?");
+console.log("Hello JavaScript");
+console.error("เจอไวรัส");
+console.warn("เจอข้อผิดพลาด");
