baabba0355479705e854c148684605037662bf44
Kawinphop Chomnikorn <kawinphop.ch@kkumail.com>
2022-08-24 23:27:55 +0700

    day1-24/08/2022

---
+++ Basic_day1_24-8-2022/app.js
@@ -0,0 +1,10 @@
+//แสดงเป็นข้อความ, ตัวเลข, หรือแท็ก HTML ก็ได้ในหน้าเว็บ
+document.write("<h1><i>kawinphop.com</i></h1>"); 
+document.write("<p>JavaScript-basic</p>");
+
+// alert("words"); สำหรับแจ้งเตือนผู้ใช้ในหน้าเว็บ
+alert("Hello, Your welcome");
+// Console.log("ข้อความ หรือ ตัวแปร") สำหรับ debug ค่าต่างๆ แต่จะไม่แสดงผลในหน้าเว็บ
+console.log("Hello JavaScript");
+console.error("เจอไวรัส อันตราย");
+console.warn("เจอข้อผิดพลาด ต้องระวัง");
