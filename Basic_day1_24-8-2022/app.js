5d0f877d8c855c6aeab021f9d7163ebff79ebcdc
Kawinphop Chomnikorn <kawinphop.ch@kkumail.com>
2022-08-28 02:50:16 +0700

    Update app.js

--- Basic_day1_24-8-2022/app.js
+++ Basic_day1_24-8-2022/app.js
@@ -4,7 +4,9 @@ document.write("<p>JavaScript-basic</p>");
 
 // alert("words"); สำหรับแจ้งเตือนผู้ใช้ในหน้าเว็บ
 alert("Hello, Your welcome");
-// Console.log("ข้อความ หรือ ตัวแปร") สำหรับ debug ค่าต่างๆ แต่จะไม่แสดงผลในหน้าเว็บ
+
+/* Console.log("ข้อความ หรือ ตัวแปร") 
+สำหรับ debug ค่าต่างๆ แต่จะไม่แสดงผลในหน้าเว็บ */
 console.log("Hello JavaScript");
 console.error("เจอไวรัส อันตราย");
 console.warn("เจอข้อผิดพลาด ต้องระวัง");
